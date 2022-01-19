5595ccce51ba29383b2af721d33de6a74a7138e9
Darkhorse03111 <darkhorse03111@gmail.com>
2022-01-20 02:46:36 +0300

    init

---
+++ __settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1301652.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 64130258, 64170076, 64170075, 64162419, 64162108, 64159266, 64170064, 64170074, 64225698, 64225699, 64237393, 64238830, 64240087, 64284442, 64293406, 64296994, 64626970 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
